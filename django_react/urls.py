3c7f9baf636b4d174bd45863d5b576b310360209
prashasy <prashasyashok@gmail.com>
2021-07-28 16:12:45 +0530

    updates structure

--- django_react/urls.py
+++ django_react/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('',include('subscriptionsAPI.urls')),
     path('', include('frontend.urls')),
+    path('',include('subscriptionsAPI.urls')),
+    
 ]
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from "react-dom";
-import SubscribePage from './components'
+import SubscribePage from './components/SubscribePage/SubscribePage';
 
 const container = document.getElementById("app");
 render(<SubscribePage />, container);
