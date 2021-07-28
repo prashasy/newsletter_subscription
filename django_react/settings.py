f830a7c46360801f767055874eb72c37dfe8daf5
prashasy <prashasyashok@gmail.com>
2021-07-28 15:32:01 +0530

    configures frontend app

--- django_react/settings.py
+++ django_react/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'subscriptionsAPI',
     'rest_framework',
+    'frontend',
 ]
 
 MIDDLEWARE = [
---
+++ frontend/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render } from "react-dom";
+import SubscribePage from './components'
+
+const container = document.getElementById("app");
+render(<SubscribePage />, container);
