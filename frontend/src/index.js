e31163ec0866c70a6759200c916036514ef5112b
prashasy <prashasyashok@gmail.com>
2021-07-30 18:29:21 +0530

    integrates API connections

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from "react-dom";
 import SubscribePage from './components/SubscribePage/SubscribePage';
+import './style.css';
 
 const container = document.getElementById("app");
 render(<SubscribePage />, container);
